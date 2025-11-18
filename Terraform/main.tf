d8cb51564d20b6fba09c67f171c7f05b7ed4b552
Prateekk2800 <Prateekkesharwani28@gmail.com>
2025-11-18 13:09:47 +0530

    tf and yml

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -11,62 +11,20 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# IAM role for SSM
-resource "aws_iam_role" "ssm_role" {
-  name = "EC2-SSM-Role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ssm_managed_policy" {
-  role       = aws_iam_role.ssm_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-resource "aws_iam_instance_profile" "ssm_profile" {
-  name = "EC2-SSM-Instance-Profile"
-  role = aws_iam_role.ssm_role.name
-}
-
-# Security group
-resource "aws_security_group" "web_sg" {
-  name   = "allow-http"
-  vpc_id = "vpc-xxxx" # UPDATE THIS
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# EC2 instance
 resource "aws_instance" "web" {
-  ami                    = "ami-0e4e4b2f01c0f4f37" # Ubuntu 22.04
-  instance_type          = "t3.micro"
-  key_name               = "LinuxKP"
-  iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  ami           = "ami-0f5ee92e2d63afc18" # Amazon Linux 2
+  instance_type = "t2.micro"
+
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+  EOF
 
   tags = {
-    Name = "SSM-EC2"
+    Name = "SimpleWebServer"
   }
 }
 

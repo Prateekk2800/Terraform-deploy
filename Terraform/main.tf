ad852495a610063b80c2ae1526f4cb7e84d17dad
Prateekk2800 <Prateekkesharwani28@gmail.com>
2025-11-18 13:44:11 +0530

    yml

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -11,18 +11,19 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# IAM Role for SSM
+# SSM IAM Role
 resource "aws_iam_role" "ssm_role" {
   name = "EC2SSMRole"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
+        Action = "sts:AssumeRole"
         Effect = "Allow"
         Principal = {
           Service = "ec2.amazonaws.com"
         }
-        Action = "sts:AssumeRole"
       }
     ]
   })
@@ -36,7 +37,7 @@ resource "aws_iam_role_policy_attachment" "ssm_attach" {
 # Security Group
 resource "aws_security_group" "web_sg" {
   name        = "web-sg"
-  description = "Allow HTTP inbound"
+  description = "Allow HTTP inbound traffic"
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
@@ -54,41 +55,25 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
-# Get default VPC
+# Use default VPC
 data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
-}
-
 # EC2 instance
 resource "aws_instance" "web" {
-  ami           = "ami-0a313d6098716f372" # Ubuntu 22.04 LTS for ap-south-1
-  instance_type = "t3.micro"
-  subnet_id     = data.aws_subnet_ids.default.ids[0]
-  security_groups = [aws_security_group.web_sg.name]
-
-  iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
+  ami                         = "ami-0de53d8956e8dcf80" # Amazon Linux 2
+  instance_type               = "t3.micro"
+  key_name                    = "LinuxKP" # replace with your key
+  iam_instance_profile        = aws_iam_instance_profile.ssm_profile.name
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
 
   tags = {
-    Name = "WebServer"
+    Name = "TerraformWebServer"
   }
 }
 
-# IAM Instance Profile
+# Instance Profile
 resource "aws_iam_instance_profile" "ssm_profile" {
-  name = "EC2SSMInstanceProfile"
-  role = aws_iam_role.ssm_role.name
-}
-
-# Outputs
-output "instance_id" {
-  value = aws_instance.web.id
-}
-
-output "public_ip" {
-  value = aws_instance.web.public_ip
-}
-
+  name = "EC2SSMProfile"
+  role = aws_iam_role.ssm_rol_

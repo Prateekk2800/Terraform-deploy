65dbbfa305c620e2759f25f966ed733ac9e2c982
Prateekk2800 <Prateekkesharwani28@gmail.com>
2025-11-18 13:18:12 +0530

    tf and yml

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,33 +1,77 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-05d53127e5aff7daf" # Amazon Linux 2
-  instance_type = "t3.micro"
+# IAM Role for SSM
+resource "aws_iam_role" "ssm_role" {
+  name = "EC2SSMRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
 
-  user_data = <<-EOF
-    #!/bin/bash
-    yum update -y
-    yum install -y httpd
-    systemctl start httpd
-    systemctl enable httpd
-  EOF
+# Attach AmazonSSMManagedInstanceCore policy
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Instance Profile for EC2
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "EC2SSMInstanceProfile"
+  role = aws_iam_role.ssm_role.name
+}
+
+# Security Group for HTTP
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "web" {
+  ami                    = "ami-0f5ee92e2d63afc18"  # Amazon Linux 2 in ap-south-1
+  instance_type          = "t2.micro"
+  iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
+  security_groups        = [aws_security_group.web_sg.name]
+  key_name               = "LinuxKP"
 
   tags = {
     Name = "SimpleWebServer"
   }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y nginx
+    systemctl enable nginx
+    systemctl start nginx
+  EOF
 }
 
 output "instance_id" {
   value = aws_instance.web.id
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
 }

d7532046f60cf8801ec34ce4dc136ff6ec2b15ca
Prateekk2800 <Prateekkesharwani28@gmail.com>
2025-11-18 14:01:40 +0530

    yml

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -11,16 +11,53 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+# Security group allowing HTTP and SSH
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance with SSM role
+resource "aws_instance" "web" {
+  ami           = "ami-0d5d9d301c853a04a" # Amazon Linux 2
+  instance_type = "t3.micro"
+  key_name      = "LinuxKP"
+  security_groups = [aws_security_group.web_sg.name]
+
+  iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
+
+  tags = {
+    Name = "WebServer"
+  }
+}
+
 # IAM Role for SSM
 resource "aws_iam_role" "ssm_role" {
   name = "EC2SSMRole"
-
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
         Principal = {
           Service = "ec2.amazonaws.com"
         }
@@ -29,62 +66,17 @@ resource "aws_iam_role" "ssm_role" {
   })
 }
 
-# Attach AmazonSSMManagedInstanceCore
 resource "aws_iam_role_policy_attachment" "ssm_attach" {
   role       = aws_iam_role.ssm_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-# Security Group allowing HTTP/SSH
-resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Allow HTTP and SSH"
-  ingress = [
-    {
-      description = "HTTP"
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      description = "SSH"
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
-  egress = [
-    {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
-}
-
-# EC2 Instance
-resource "aws_instance" "web" {
-  ami           = "ami-0dba2cb6798deb6d8" # Amazon Linux 2
-  instance_type = "t3.micro"
-  key_name      = "LinuxKP"              # your key pair
-  iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-
-  tags = {
-    Name = "TerraformWebServer"
-  }
-}
-
-# IAM Instance Profile for EC2
 resource "aws_iam_instance_profile" "ssm_profile" {
-  name = "EC2SSMInstanceProfile"
+  name = "EC2SSMProfile"
   role = aws_iam_role.ssm_role.name
 }
 
-# Output instance ID and public IP
+# Outputs
 output "instance_id" {
   value = aws_instance.web.id
 }

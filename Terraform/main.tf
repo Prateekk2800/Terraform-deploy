f6e705f3867d9f346d6d4462390bdfea52a3af4b
Prateekk2800 <Prateekkesharwani28@gmail.com>
2025-11-18 13:35:54 +0530

    yml

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
@@ -7,38 +16,36 @@ resource "aws_iam_role" "ssm_role" {
   name = "EC2SSMRole"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
       }
-    }]
+    ]
   })
 }
 
-# Attach AmazonSSMManagedInstanceCore policy
 resource "aws_iam_role_policy_attachment" "ssm_attach" {
   role       = aws_iam_role.ssm_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-# Instance Profile for EC2
-resource "aws_iam_instance_profile" "ssm_profile" {
-  name = "EC2SSMInstanceProfile"
-  role = aws_iam_role.ssm_role.name
-}
-
-# Security Group for HTTP
+# Security Group
 resource "aws_security_group" "web_sg" {
   name        = "web-sg"
-  description = "Allow HTTP"
+  description = "Allow HTTP inbound"
+  vpc_id      = data.aws_vpc.default.id
+
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -47,27 +54,36 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
-# EC2 Instance
-resource "aws_instance" "web" {
-  ami                    = "ami-0f5ee92e2d63afc18"  # Amazon Linux 2 in ap-south-1
-  instance_type          = "t2.micro"
-  iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
-  security_groups        = [aws_security_group.web_sg.name]
-  key_name               = "LinuxKP"
-
-  tags = {
-    Name = "SimpleWebServer"
-  }
-
-  user_data = <<-EOF
-    #!/bin/bash
-    yum update -y
-    yum install -y nginx
-    systemctl enable nginx
-    systemctl start nginx
-  EOF
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
 }
 
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+# EC2 instance
+resource "aws_instance" "web" {
+  ami           = "ami-0a313d6098716f372" # Ubuntu 22.04 LTS for ap-south-1
+  instance_type = "t3.micro"
+  subnet_id     = data.aws_subnet_ids.default.ids[0]
+  security_groups = [aws_security_group.web_sg.name]
+
+  iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
+
+  tags = {
+    Name = "WebServer"
+  }
+}
+
+# IAM Instance Profile
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "EC2SSMInstanceProfile"
+  role = aws_iam_role.ssm_role.name
+}
+
+# Outputs
 output "instance_id" {
   value = aws_instance.web.id
 }
@@ -75,3 +91,4 @@ output "instance_id" {
 output "public_ip" {
   value = aws_instance.web.public_ip
 }
+

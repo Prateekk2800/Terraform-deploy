32b16ce768eb4ac1b24a2baa8b3458cdcdf77de0
Prateekk2800 <Prateekkesharwani28@gmail.com>
2025-11-18 13:02:56 +0530

    tf and yml

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -11,59 +11,38 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# ----------------------------------
-# Get default VPC
-# ----------------------------------
-data "aws_vpc" "default" {
-  default = true
-}
-
-# ----------------------------------
-# Get default subnet
-# ----------------------------------
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-# ----------------------------------
-# IAM ROLE & INSTANCE PROFILE (for SSM)
-# ----------------------------------
-resource "aws_iam_role" "ec2_ssm_role" {
+# IAM role for SSM
+resource "aws_iam_role" "ssm_role" {
   name = "EC2-SSM-Role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-      Action    = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
     }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ssm_core" {
-  role       = aws_iam_role.ec2_ssm_role.name
+resource "aws_iam_role_policy_attachment" "ssm_managed_policy" {
+  role       = aws_iam_role.ssm_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-resource "aws_iam_instance_profile" "ec2_ssm_profile" {
-  name = "EC2-SSM-InstanceProfile"
-  role = aws_iam_role.ec2_ssm_role.name
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "EC2-SSM-Instance-Profile"
+  role = aws_iam_role.ssm_role.name
 }
 
-# ----------------------------------
-# Security Group (HTTP + Outbound)
-# ----------------------------------
+# Security group
 resource "aws_security_group" "web_sg" {
-  name        = "ec2-web-sg"
-  description = "Allow HTTP"
-  vpc_id      = data.aws_vpc.default.id
+  name   = "allow-http"
+  vpc_id = "vpc-xxxx" # UPDATE THIS
 
   ingress {
-    description = "Allow HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -78,23 +57,19 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
-# ----------------------------------
-# EC2 INSTANCE
-# ----------------------------------
-resource "aws_instance" "myserver" {
-  ami           = "ami-0f5ee92e2d63afc18"  # Ubuntu 22.04 LTS (ap-south-1)
-  instance_type = "t2.micro"
-
-  key_name = "LinuxKP"
-
-  subnet_id = data.aws_subnets.default.ids[0]
-
-  iam_instance_profile = aws_iam_instance_profile.ec2_ssm_profile.name
-  vpc_security_group_ids = [
-    aws_security_group.web_sg.id
-  ]
+# EC2 instance
+resource "aws_instance" "web" {
+  ami                    = "ami-0e4e4b2f01c0f4f37" # Ubuntu 22.04
+  instance_type          = "t3.micro"
+  key_name               = "LinuxKP"
+  iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
-    Name = "MyServer"
+    Name = "SSM-EC2"
   }
+}
+
+output "instance_id" {
+  value = aws_instance.web.id
 }
